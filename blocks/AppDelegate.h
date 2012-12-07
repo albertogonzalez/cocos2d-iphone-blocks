7afeaca1cd04eab356f6f53949c29c557126479b
Alberto Gonzalez <dev@albertogonzalez.net>
2012-12-07 16:32:05 +0100

    initial project from cocos2d template

---
+++ blocks/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  blocks
+//
+//  Created by Alberto Gonzalez on 07/12/12.
+//  Copyright albertogonzalez.net 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
