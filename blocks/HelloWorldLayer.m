4594d46037b800fc606163ef2e88a97848427ef2
Alberto Gonzalez <dev@albertogonzalez.net>
2012-12-07 17:52:20 +0100

    added blocks

--- blocks/HelloWorldLayer.m
+++ blocks/HelloWorldLayer.m
@@ -13,10 +13,24 @@
 // Needed to obtain the Navigation Controller
 #import "AppDelegate.h"
 
+#define BACKGROUND_Z	0
+#define BLOCKS_Z		1
+#define BLOCKS_TAG		1
+#define BLOCKS_ROWS		4
+#define BLOCKS_COLS		5
+#define BLOCKS_MAX		20
+
+@interface HelloWorldLayer()
+@property (nonatomic, retain) NSMutableArray *blocks;
+@end
+
+
 #pragma mark - HelloWorldLayer
 
 // HelloWorldLayer implementation
 @implementation HelloWorldLayer
+
+@synthesize blocks = _blocks;
 
 // Helper class method that creates a Scene with the HelloWorldLayer as the only child.
 +(CCScene *) scene
@@ -42,13 +56,33 @@
 	if( (self=[super init]) ) {
 		
 		// ask director for the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
+		CGSize winSize = [[CCDirector sharedDirector] winSize];
 		
 		// background
 		CCSprite *background = [CCSprite spriteWithFile:@"background.png"];
-		background.position =  ccp( size.width/2 , size.height/2 );
-		[self addChild:background];
+		background.position =  ccp( winSize.width/2 , winSize.height/2 );
+		[self addChild:background z:BACKGROUND_Z];
 		
+		// block
+		CCSprite *block = [CCSprite spriteWithFile:@"block.png"];
+		CGSize blockSize = block.contentSize;
+		self.blocks = [NSMutableArray arrayWithCapacity:BLOCKS_MAX];
+		CGPoint blockPos;
+		blockPos.y = winSize.height - blockSize.height/2;
+		for (int row = 0; row < BLOCKS_ROWS; row++)
+		{
+			blockPos.x = blockSize.width/2;
+			for (int col = 0; col < BLOCKS_COLS; col++)
+			{
+				CCSprite *block = [CCSprite spriteWithFile:@"block.png"];
+				block.position =  blockPos;
+				[self addChild:block z:BLOCKS_Z tag:BLOCKS_TAG];
+				[self.blocks addObject:block];
+				
+				blockPos.x += blockSize.width;
+			}
+			blockPos.y -= blockSize.height;
+		}
 	}
 	return self;
 }
@@ -59,6 +93,8 @@
 	// in case you have something to dealloc, do it in this method
 	// in this particular example nothing needs to be released.
 	// cocos2d will automatically release all the children (Label)
+	
+	[_blocks release];
 	
 	// don't forget to call "super dealloc"
 	[super dealloc];
